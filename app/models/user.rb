39ef6631c3a1556c4cebc62129d72096908896b7
Marco Lau <lau.career@gmail.com>
2014-03-12 17:06:46 +0800

    Added encrypt_password in user.rb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,21 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  attr_accessor :password, :password_confirmation
+
   field :email, type: String
   field :salt, type: String
   field :fish, type: String
+
+  before_save :encrypt_password
+
+  protected
+
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.fish = BCrypt::Engine.hash_secret(password, self.salt)
+    end
+  end
 
 end
