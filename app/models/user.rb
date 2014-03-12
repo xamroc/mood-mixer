495252cf7c3c9dde7d59d9b0b5f2635b925db6a1
Marco Lau <lau.career@gmail.com>
2014-03-12 17:50:13 +0800

    Added self.authenticate in user.rb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,11 @@ class User
 
   before_save :encrypt_password
 
+  def self.authenticate(email, password)
+    user = User.find_by email: email
+    user if user and user.authenticate(password)
+  end
+
   def authenticate(password)
     self.fish == BCrypt::Engine.hash_secret(password, self.salt)
   end
