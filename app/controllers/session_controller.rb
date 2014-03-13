f8045bf4c1db56e159755930aeb70e7acf34b6f8
Marco Lau <lau.career@gmail.com>
2014-03-13 17:12:55 +0800

    Added authentication case.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,7 +5,14 @@ class SessionController < ApplicationController
   end
 
   def create
-    render text: User.authenticate(params[:user][:email], params[:user][:password]).email
+    @user = User.authenticate(params[:user][:email], params[:user][:password])
+
+    if @user
+      session[:user_id] = @user_id
+      render text: "Logged in yo! #{@user.email}"
+    else
+      render text: "Who are you?"
+    end
   end
 
   def destroy
