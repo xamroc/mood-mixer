6215e7044e62110e75eceff5e892e4c55b46fb7f
Marco Lau <lau.career@gmail.com>
2014-03-14 10:57:51 +0800

    Added is_authenticated?.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,13 +9,15 @@ class SessionController < ApplicationController
 
     if @user
       session[:user_id] = @user_id
-      render text: "Logged in yo! #{@user.email}"
+      # render text: "Logged in yo! #{@user.email}"
+      redirect_to root_url
     else
       render text: "Who are you?"
     end
   end
 
   def destroy
+    session[:user_id] = nil
     render text: "Log the user out."
   end
 
